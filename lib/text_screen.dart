e6885d44cf1094979db64761f306cba4beb382f3
Nidham Kacha <nidham78@gmail.com>
2024-02-07 16:52:21 +0530

    7-2-24
    Signed-off-by: Nidham Kacha <nidham78@gmail.com>

--- lib/text_screen.dart
+++ lib/text_screen.dart
@@ -19,81 +19,156 @@ class _TextScreenState extends State<TextScreen> {
           backgroundColor: Colors.blueAccent,
         ),
         // Body
-        body: Column(
-          children: [
-            Align(
-              alignment: Alignment.center,
-              child: Text(
-                "Past Tours",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-              ),
-            ),
-            Column(
-              children: [
-                Container(
-                  margin: EdgeInsets.only(top: 30),
-                  height: 400,
-                  width: 370,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      border: Border.all(color: Colors.red)),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          "Mon,Apr 4,10 AM to 11:45 AM",
-                          style: TextStyle(fontWeight: FontWeight.w500),
-                        ),
-                      ),
-                      Text(
-                        "Cancelled",
-                        style: TextStyle(color: Colors.red),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Text(
-                            "You Can Request Another Tour Anytime.If You Have Question Please Contact Your Agent."),
-                      ),
-                      Container(
-                        height: 200,
-                        width: 340,
-                        decoration: BoxDecoration(
-                            color: Colors.amber,
-                            borderRadius: BorderRadius.circular(20)),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Text(
-                              "Goldem Meadows",
-                              style: TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.w600),
-                            ),
-                            Text("Price")
-                          ],
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Text(
-                              "Goldem Meadows",
-                            ),
-                            Text("Price")
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  "Past Tours",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
                 ),
-              ],
-            )
-          ],
+              ),
+              Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 30),
+                    height: 400,
+                    width: 370,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        border: Border.all(color: Colors.black)),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            "Mon,Apr 4,10 AM to 11:45 AM",
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        Text(
+                          "✘ Cancelled",
+                          style: TextStyle(color: Colors.red),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Text(
+                              "You Can Request Another Tour Anytime.If You Have Question Please Contact Your Agent."),
+                        ),
+                        Container(
+                          height: 200,
+                          width: 340,
+                          decoration: BoxDecoration(
+                              color: Colors.amber,
+                              borderRadius: BorderRadius.circular(20)),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text(
+                                "Goldem Meadows",
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.w600),
+                              ),
+                              Text("Price")
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(2.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text(
+                                "📍St. Celine, Delaware 10299",
+                              ),
+                              Text(
+                                "\$436",
+                                style: TextStyle(
+                                    color: Colors.blue,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 30),
+                    height: 400,
+                    width: 370,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        border: Border.all(color: Colors.black)),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            "Mon,Apr 4,10 AM to 11:45 AM",
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        Text(
+                          "✔️ Complete",
+                          style: TextStyle(color: Colors.green),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Text(
+                              "You Can Request Another Tour Anytime.If You Have Question Please Contact Your Agent."),
+                        ),
+                        Container(
+                          height: 200,
+                          width: 340,
+                          decoration: BoxDecoration(
+                              color: Colors.amber,
+                              borderRadius: BorderRadius.circular(20)),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text(
+                                "Goldem Meadows",
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.w600),
+                              ),
+                              Text("Price")
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(2.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text(
+                                "📍St. Celine, Delaware 10299",
+                              ),
+                              Text(
+                                "\$436",
+                                style: TextStyle(
+                                    color: Colors.blue,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
