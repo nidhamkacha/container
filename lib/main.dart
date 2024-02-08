5be3eef836f373df83969241d545cfef981cb235
vijay vekariya <vijayvekariya007@gmail.com>
2024-02-08 14:41:19 +0530

    7-2-24

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   @override
-  void initState() {
+  void initState() {  
     super.initState();
   }
 
--- lib/text_screen.dart
+++ lib/text_screen.dart
@@ -43,7 +43,7 @@ class _TextScreenState extends State<TextScreen> {
                         Padding(
                           padding: const EdgeInsets.all(8.0),
                           child: Text(
-                            "Mon,Apr 4,10 AM to 11:45 AM",
+                            "🗓️ Mon,Apr 4, 🕝 10 AM to 11:45 AM",
                             style: TextStyle(fontWeight: FontWeight.w500),
                           ),
                         ),
@@ -165,8 +165,77 @@ class _TextScreenState extends State<TextScreen> {
                       ],
                     ),
                   ),
+                  Container(
+                    margin: EdgeInsets.only(top: 30),
+                    height: 400,
+                    width: 370,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        border: Border.all(color: Colors.black)),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            "Mon,Apr 4,10 AM to 11:45 AM",
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        Text(
+                          "⏳ Pending",
+                          style: TextStyle(color: Colors.green),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Text(
+                              "You Can Request Another Tour Anytime.If You Have Question Please Contact Your Agent."),
+                        ),
+                        Container(
+                          height: 200,
+                          width: 340,
+                          decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                  colors: [Colors.red, Colors.yellow],
+                                  transform: GradientRotation(20)),
+                              borderRadius: BorderRadius.circular(20)),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text(
+                                "Goldem Meadows",
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.w600),
+                              ),
+                              Text("Price")
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(2.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text(
+                                "📍St. Celine, Delaware 10299",
+                              ),
+                              Text(
+                                "\$436",
+                                style: TextStyle(
+                                    color: Colors.blue,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
                 ],
-              )
+              ),
             ],
           ),
         ),
